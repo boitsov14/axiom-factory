7d3053204b70a8dd62ac384754f34511b7a01add
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2026-06-21 07:05:12 +0900

    wip

--- logic/src/proof.rs
+++ logic/src/proof.rs
@@ -1,29 +1,7 @@
 use crate::syntax::{Formula, Id, Sort};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Hyp {
-    pub id: Id,
-    pub formula: Formula,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Default)]
-pub struct Context {
-    pub vars: Vec<(Id, Sort)>,
-    pub hyps: Vec<Hyp>,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Goal {
-    pub ctx: Context,
+    pub hypotheses: Vec<Formula>,
     pub target: Formula,
-}
-
-impl Goal {
-    /// 空の `Context` を持つ `Goal` を作る。
-    pub fn new(target: Formula) -> Self {
-        Self {
-            ctx: Context::default(),
-            target,
-        }
-    }
 }
